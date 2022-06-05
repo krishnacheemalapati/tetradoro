32e47bd540d33a53c769ecf2311f2235382616b5
krishnacheemalapati <krishkrishct@gmail.com>
2022-06-05 07:36:16 -0400

    everything else

--- app.js
+++ app.js
@@ -1,4 +1,115 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let minutes = 25
+    let seconds = 0
+    let minText = '25'
+    let secText = '00'
+    let timerInterval
+    let toggled = 'doro'
+    const backgrounds = [
+        'url(./assets/beach1.jpg)',
+        'url(./assets/beach2.webp)',
+        'url(./assets/beach3.jpg)',
+        'url(./assets/beach4.jpg)',
+        'url(./assets/beach5.jpg)',
+        'url(./assets/beach6.jpg)',
+    ]
+    const back = document.querySelector('html')
+    const toggleText = document.querySelector('#tetradoro-text')
+    toggleText.addEventListener('click', tetradoroClick)
+
+    const pomButton = document.getElementById("pom-button")
+    pomButton.addEventListener("click", () => {
+      const { pom } = pomButton.dataset
+      if (pom === "start") {
+        startCountdown()
+      } else {
+        stopCountdown()
+      }
+    })
+
+    function calcTimeLeft(endTime) {
+        const currentTime = Date.parse(new Date())
+        const difference = endTime - currentTime
+        minutes = Number.parseInt((difference / 60000) % 60, 10)
+        seconds = Number.parseInt((difference / 1000) % 60, 10)
+        minText = Number(minutes).toString().padStart(2, '0')
+        secText = Number(seconds).toString().padStart(2, '0')
+    }
+
+    function startCountdown() {
+        const end = Date.parse(new Date()) + minutes * 60 * 1000;
+        pomButton.dataset.pom = "stop"
+        pomButton.classList.add("active")
+        startButton.classList.add("active")
+      
+        timerInterval = setInterval(function() {
+          calcTimeLeft(end)
+          updateTimeLeft()
+      
+          total = minutes * 60 + seconds
+          if (minutes % 5 == 0 && seconds % 60 == 0) {
+              bgIndex = Math.floor(Math.random()*backgrounds.length)
+              back.style.background = backgrounds[bgIndex]
+          }
+          if (total <= 0) {
+            clearInterval(timerInterval)
+            clearInterval(timerId)
+            timerId = null
+            toggled = (toggled.valueOf() == "tetra") ? "doro" : "tetra"
+            tetradoroToggle(toggled)
+            startCountdown()
+          }
+        }, 1000)
+      }
+
+    function stopCountdown() {
+      clearInterval(timerInterval);
+      pomButton.dataset.pom = "start";
+      pomButton.classList.remove("active");
+      startButton.classList.remove("active");      
+    }
+
+    function updateTimeLeft() {      
+        document.querySelector('#game-time-left').innerHTML = minText+":"+secText
+        document.querySelector('#pom-time-left').innerHTML = minText+":"+secText
+    }
+
+    function tetradoroToggle(text) {
+        minutes = (text.valueOf() == "tetra") ? 5 : 25
+        seconds = 0
+        minText = Number(minutes).toString().padStart(2, '0')
+        secText = Number(seconds).toString().padStart(2, '0')
+        console.log(minutes)
+        document
+          .querySelectorAll('[data-text]')
+          .forEach(e => e.classList.remove('active'))
+        document.querySelector(`[data-text="${text}"]`).classList.add('active')
+        if (minutes < 10) {
+            minText = '0'+minutes.toString()
+        }
+        if (seconds < 10) {
+            secText = '0'+seconds.toString()
+        }
+        updateTimeLeft()
+    }
+
+    function tetradoroClick(event) {
+      const { text } = event.target.dataset
+      if (!text) return
+      toggled = text
+      tetradoroToggle(text)
+      stopCountdown()
+      if (text.valueOf() == "tetra") {
+        document.querySelector('.pomodoro-container').classList.add('invisible')
+        document.querySelector('.tetris-container').classList.remove('invisible')
+        document.querySelector('.tetradoro-text').style.marginTop = "0vh"
+      } else {
+        document.querySelector('.tetris-container').classList.add('invisible')
+        document.querySelector('.pomodoro-container').classList.remove('invisible')
+        document.querySelector('.tetradoro-text').style.marginTop = "10vh"
+      }
+    }
+    
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
     const column = 20
@@ -17,6 +128,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const ee = 69
     let upcomingI = 0
     let timerId
+    let interval
     let score = 0
     const images = [
         'url(./assets/lightblue.png)', 
@@ -26,8 +138,8 @@ document.addEventListener('DOMContentLoaded', () => {
         'url(./assets/limegreen.png)', 
         'url(./assets/mediumpurple.png)', 
         'url(./assets/tomato.png)',
-        'url(./assets/white.png)'
     ]
+    gameEnded = false
 
 
     const scoreDisplay = document.querySelector('#score')
@@ -101,7 +213,7 @@ document.addEventListener('DOMContentLoaded', () => {
         currPiece.forEach(index => {
             squares[currPos + index].classList.remove('piece')
             squares[currPos + index].style.backgroundColor = ''
-            squares[currPos + index].style.content = images[7]
+            squares[currPos + index].style.content = ''
         })
     }
 
@@ -193,8 +305,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function updateUpcoming() {
         upcomingSquares.forEach(square => {
             square.classList.remove('piece')
-            square.style.backgroundColor = ''
-            square.style.backgroundImage = images[7]
+            square.style.content = ''
         })
         upcomingPieces[upcomingI].forEach(index => {
             upcomingSquares[upcomingIndex + index].classList.add('piece')
@@ -204,13 +315,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     startButton.addEventListener('click', () => {
         if (timerId) {
+            stopCountdown()
             clearInterval(timerId)
             timerId = null
         } else {
+            startCountdown()
             draw()
             timerId = setInterval(down, 1000)
             upcomingI = Math.floor(Math.random()*pieces.length)
             updateUpcoming()
+        }
+        if (gameEnded) {
+            squares.forEach(square => {
+                squares[index].classList.remove('piece')
+                squares[index].classList.remove('secret')
+                squares[index].style.content = ''
+            gameEnded = false
+            })
         }
     })
 
@@ -224,8 +345,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 row.forEach(index => {
                     squares[index].classList.remove('piece')
                     squares[index].classList.remove('secret')
-                    squares[index].style.backgroundColor = ''
-                    squares.style.content = images[7]
+                    squares[index].style.content = ''
                 })
                 const removed = squares.splice(i, row1)
                 squares = removed.concat(squares)
@@ -236,8 +356,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function endGame() {
         if(currPiece.some(index => squares[currPos + index].classList.contains('secret'))) {
-            scoreDisplay.innerHTML = 'GAME OVER'
+            scoreDisplay.innerHTML = score + '\nGame Over!'
             clearInterval(timerId)
+            gameEnded = true
         }
     }
 
